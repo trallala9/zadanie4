f8186351dbb6ccb9b29993ea57fcc53650950c05
Karolina Siwkowska <trallala00@gmail.com>
2017-12-30 21:59:01 +0100

    Init project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var a = 6;
+var b = 2;
+var value = (a * a) - (2 * a * b) + (b * b)
+if (value > 0) {
+    console.log('Wynik dodatni');
+} else if (value < 0) {
+    console.log('Wynik ujemny');
+} else {
+    console.log('Wynik zero');
+}
